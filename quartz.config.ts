172e4c3482823101fb21187b967c756f8f5d21b5
Crynoore <cid.serverius@gmail.com>
2025-07-25 17:57:07 +0200

    Quartz sync: Jul 25, 2025, 5:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "CryBrew DnD",
+    pageTitle: "Tale of the Titan's",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
